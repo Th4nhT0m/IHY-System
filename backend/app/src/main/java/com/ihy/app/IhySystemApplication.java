7f1f129cf48eed97cd3f5e0049680d0c6d1edae5
Trần Tuấn Thành <tomcoder0129@gmail.com>
2025-10-16 12:03:03 +0700

    Project_Initialization

---
+++ backend/app/src/main/java/com/ihy/app/IhySystemApplication.java
@@ -0,0 +1,13 @@
+package com.ihy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IhySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IhySystemApplication.class, args);
+	}
+
+}
