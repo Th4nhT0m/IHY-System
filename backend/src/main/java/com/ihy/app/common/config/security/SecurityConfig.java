efab0b9aa8ffafc2169bcab0087d67b21a631dfa
Trần Tuấn Thành <tomcoder0129@gmail.com>
2025-11-18 16:53:46 +0700

    Config Spring Security

---
+++ backend/src/main/java/com/ihy/app/common/config/security/SecurityConfig.java
@@ -0,0 +1,72 @@
+package com.ihy.app.common.config.security;
+
+import lombok.AccessLevel;
+import lombok.experimental.FieldDefaults;
+import lombok.experimental.NonFinal;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.crypto.spec.SecretKeySpec;
+
+@Configuration
+@EnableWebSecurity
+@FieldDefaults(level = AccessLevel.PUBLIC, makeFinal = true)
+public class SecurityConfig {
+
+    @NonFinal
+    @Value("${jwt.signerKey}")
+    protected String SIGN_KEY;
+
+    public static final String[] PUBLIC_ACCESS = {
+            "/user/register",
+            "/auth/login",
+            "/auth/introspect"
+    };
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        // Configure authorization rules for incoming HTTP requests.
+        httpSecurity.authorizeHttpRequests(request -> {
+            request.requestMatchers(HttpMethod.POST, PUBLIC_ACCESS).permitAll()
+                    .anyRequest().authenticated();
+        });
+
+        // Configure the application as an OAuth2 Resource Server using JWT for authentication
+        httpSecurity.oauth2ResourceServer(resourceServer -> {
+            resourceServer.jwt( jwtConfigurer -> {
+                jwtConfigurer.decoder(jwtDecoder());
+            });
+        });
+
+        //TODO
+        httpSecurity.csrf(csrf -> csrf.disable());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    JwtDecoder jwtDecoder(){
+        SecretKeySpec secretKey = new SecretKeySpec(SIGN_KEY.getBytes(),"HS256");
+        // This decoder will be responsible for validating and decoding incoming JWT tokens.
+        return NimbusJwtDecoder
+                .withSecretKey(secretKey)
+                .macAlgorithm(MacAlgorithm.HS256)
+                .build();
+    }
+
+}
